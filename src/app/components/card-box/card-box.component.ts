fdc1715fe2b35d12527743aff51864faac13e732
antoniovictor2k <avps2@aluno.ifal.edu.br>
2023-07-26 14:27:10 -0300

    att

--- src/app/components/card-box/card-box.component.ts
+++ src/app/components/card-box/card-box.component.ts
@@ -1,23 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AccountDataModel } from 'src/app/model/accountDataModel';
+import { CardsService } from 'src/app/services/cards.service';
 
 @Component({
   selector: 'app-card-box',
   templateUrl: './card-box.component.html',
   styleUrls: ['./card-box.component.css']
 })
-export class CardBoxComponent {
 
-  accountData: AccountDataModel ={
-    name: "Antonio Victor P S",
+export class CardBoxComponent implements OnInit {
+
+  constructor(private services: CardsService){}
+
+  
+  accountData: AccountDataModel  ={
+    name: "",
     account:{
-        agency: "0001",
-        number: "124541454"
+      agency: "0001",
+      number: "124541454"
     },
     card:{
-        limit: 1500,
-        number: "4454"
+      limit: 1500,
+      number: "4454"
     }
   }
+  ngOnInit(): void {
+   this.getAccountData();
+  }
 
+  getAccountData(){
+    this.services.getCard().subscribe( data =>{
+      this.accountData.name = data.name
+      this.accountData.account.agency = data.account.agency
+      this.accountData.account.number = data.account.number
+      this.accountData.card.limit = data.card.limit
+      this.accountData.card.number = data.card.number.split(" ")[3]
+    })
+  }
+  
 }
