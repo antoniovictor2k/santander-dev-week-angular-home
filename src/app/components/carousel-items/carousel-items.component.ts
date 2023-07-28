9eacde992ed5277ec78af86e57c0be45c7e622ee
antoniovictor2k <avps2@aluno.ifal.edu.br>
2023-07-27 23:49:28 -0300

    tt

--- src/app/components/carousel-items/carousel-items.component.ts
+++ src/app/components/carousel-items/carousel-items.component.ts
@@ -1,4 +1,12 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import {
+  Component,
+  Input,
+  OnDestroy,
+  OnInit,
+} from '@angular/core';
+import { Subscription, timer } from 'rxjs';
+
+
 
 @Component({
   selector: 'app-carousel-items',
@@ -7,59 +15,55 @@ import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 })
 export class CarouselItemsComponent {
   
-  @ViewChild('slickCarousel') slickCarousel!: ElementRef;
+  // [imagens]="[
+  //   '../../../assets/imgSlider/santander-img-2.jpg',
+  //   '../../../assets/imgSlider/santander-img-2.jpg',
+  //   '../../../assets/imgSlider/santander-img-2.jpg'
+  // ]"
+  // // Guarda a referência do temporizador.
+  // // Assim conseguimos interromper o temporizador
+  // // a qualquer momento
+  // timerSubs!: Subscription;
 
- 
+  // // Array com a URL das imagens que serão exibidas
+  // // no carrossel
+  // @Input() imagens: string[] = [];
 
+  // // Guarda a posição no array "imagens" que
+  // // corresponde a imagem que está sendo exibida
+  // // no carrossel
+  // private _indexImagemAtiva: number = 0;
+  // get indexImagemAtiva() {
+  //   return this._indexImagemAtiva;
+  // }
 
-    slideConfig = {
-      slidesToShow: 5, // Mostrar 5 cards por vez
-      slidesToScroll: 1,
-      nextArrow: '<div class="slick-next"><i class="fa fa-chevron-right"></i></div>',
-      prevArrow: '<div class="slick-prev"><i class="fa fa-chevron-left"></i></div>',
-      responsive: [
-        {
-          breakpoint: 1200,
-          settings: {
-            slidesToShow: 4 // Mostrar 4 cards por vez em dispositivos com largura menor que 1200px
-          }
-        },
-        {
-          breakpoint: 992,
-          settings: {
-            slidesToShow: 3 // Mostrar 3 cards por vez em dispositivos com largura menor que 992px
-          }
-        },
-        {
-          breakpoint: 768,
-          settings: {
-            slidesToShow: 1 // Mostrar 1 card por vez em dispositivos com largura menor que 768px
-          }
-        }
-      ]
-    };
-  
+  // set indexImagemAtiva(value: number) {
+  //   this._indexImagemAtiva =
+  //     value < this.imagens.length ? value : 0;
+  // }
 
-  cards: any[] = [
-    { title: 'Card 1', content: 'Conteúdo do Card 1', active: false },
-    { title: 'Card 2', content: 'Conteúdo do Card 2', active: false },
-    { title: 'Card 3', content: 'Conteúdo do Card 3', active: false },
-    { title: 'Card 4', content: 'Conteúdo do Card 4', active: false },
-    { title: 'Card 5', content: 'Conteúdo do Card 5', active: false },
-    { title: 'Card 6', content: 'Conteúdo do Card 5', active: false },
-    { title: 'Card 7', content: 'Conteúdo do Card 5', active: false },
-    // Adicione mais cards aqui
-  ];
-  nextSlide() {
-    this.slickCarousel.nativeElement.slickNext();
-  }
-  constructor() {}
+  // ngOnInit(): void {
+  //   this.iniciarTimer();
+  // }
 
-  ngOnInit(): void {}
+  // ngOnDestroy(): void {
+  //   this.pararTimer();
+  // }
 
-  toggleCard(card: any) {
-    this.cards.forEach((c) => (c.active = false));
-    card.active = true;
-  }
+  // iniciarTimer(): void {
+  //   this.timerSubs = timer(1000).subscribe(() => {
+  //     this.ativarImagem(
+  //       this.indexImagemAtiva + 1
+  //     );
+  //   });
+  // }
 
+  // pararTimer(): void {
+  //   this.timerSubs?.unsubscribe();
+  // }
+
+  // ativarImagem(index: number): void {
+  //   this.indexImagemAtiva = index;
+  //   this.iniciarTimer();
+  // }
 }
