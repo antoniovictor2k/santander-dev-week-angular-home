9586a7c0e8d3f8a6f2dc2e3b2fec3a66c1284997
antoniovictor2k <avps2@aluno.ifal.edu.br>
2023-07-28 02:16:27 -0300

    add

--- src/app/components/carousel-items/carousel-items.component.ts
+++ src/app/components/carousel-items/carousel-items.component.ts
@@ -27,25 +27,8 @@ export class CarouselItemsComponent {
     { img: 'https://via.placeholder.com/600.png/422/fff' },
     { img: 'https://via.placeholder.com/600.png/654/fff' },
   ];
-  slideConfig = { slidesToShow: 4, slidesToScroll: 4 };
-  addSlide() {
-    this.slides.push({ img: 'http://placehold.it/350x150/777777' });
-  }
-  removeSlide() {
-    this.slides.length = this.slides.length - 1;
-  }
-  slickInit(e: any) {
-    console.log('slick initialized');
-  }
-  breakpoint(e: any) {
-    console.log('breakpoint');
-  }
-  afterChange(e: any) {
-    console.log('afterChange');
-  }
-  beforeChange(e: any) {
-    console.log('beforeChange');
-  }
+  // slideConfig = { slidesToShow: 4, slidesToScroll: 4 };
+ 
   constructor() {}
   ngOnInit(): void {}
 }
